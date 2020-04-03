14cd2c342024ac8b73db2c45c5564253c9dd44b1
const-ae <artjom31415@googlemail.com>
2020-04-03 12:51:49 +0200

    Import single function from matrixStats to suppress warning

--- R/sparseMatrixStats-package.R
+++ R/sparseMatrixStats-package.R
@@ -3,6 +3,12 @@
 ## usethis namespace: end
 NULL
 
+# NOTE: Import a single function to quieten R CMD check NOTE:
+#         Package in Depends field not imported from: ‘matrixStats’
+#           These packages need to be imported from (in the NAMESPACE file)
+#           for when this namespace is loaded but not attached.
+#' @importFrom matrixStats allocArray
+NULL
 
 
 #' @useDynLib sparseMatrixStats
