92f13460ed363fe8994658f746f49406142d7d1b
const-ae <artjom31415@googlemail.com>
2020-09-22 13:46:28 +0200

    Cache iterator.end() explicitly to speed row methods
    * This addresses issue (/challenge) in #5

--- src/row_methods.cpp
+++ src/row_methods.cpp
@@ -42,7 +42,9 @@ NumericVector dgCMatrix_rowMeans2(S4 matrix, bool na_rm){
 
   double* val_iter = values.begin();
   int* idx_iter = row_indices.begin();
-  while(val_iter != values.end() && idx_iter != row_indices.end()){
+  auto val_end = values.end();
+  auto row_end = row_indices.end();
+  while(val_iter != val_end && idx_iter != row_end){
     if(na_rm && ISNA(*val_iter)){
       nas_per_row[*idx_iter] += 1;
     }else{
@@ -52,12 +54,15 @@ NumericVector dgCMatrix_rowMeans2(S4 matrix, bool na_rm){
     ++idx_iter;
   }
   auto res_iter = result.begin();
+  auto res_end = result.end();
   auto na_count_iter = nas_per_row.begin();
-  while(res_iter != result.end() && na_count_iter != nas_per_row.end()){
+  auto na_count_end = nas_per_row.end();
+  while(res_iter != res_end && na_count_iter != na_count_end){
     *res_iter = *res_iter / (dim[1] - *na_count_iter);
     ++res_iter;
     ++na_count_iter;
   }
+
   return wrap(result);
 }
 
@@ -76,8 +81,10 @@ NumericVector dgCMatrix_rowVars(S4 matrix, bool na_rm){
   std::vector<int> zeros_per_row (dim[0], dim[1]);
 
   double* val_iter = values.begin();
+  auto val_end = values.end();
   int* idx_iter = row_indices.begin();
-  while(val_iter != values.end() && idx_iter != row_indices.end()){
+  auto idx_end = row_indices.end();
+  while(val_iter != val_end && idx_iter != idx_end){
     if(na_rm && ISNA(*val_iter)){
       nas_per_row[*idx_iter] += 1;
     }else{
@@ -89,13 +96,17 @@ NumericVector dgCMatrix_rowVars(S4 matrix, bool na_rm){
     ++idx_iter;
   }
   auto res_iter = result.begin();
+  auto res_end = result.end();
   auto na_count_iter = nas_per_row.begin();
+  auto na_count_end = nas_per_row.end();
   auto zero_count_iter = zeros_per_row.begin();
+  auto zero_count_end = zeros_per_row.end();
   auto mean_iter = means.begin();
-  while(res_iter != result.end() &&
-        na_count_iter != nas_per_row.end() &&
-        zero_count_iter != zeros_per_row.end() &&
-        mean_iter != means.end()){
+  auto mean_end = means.end();
+  while(res_iter != res_end &&
+        na_count_iter != na_count_end &&
+        zero_count_iter != zero_count_end &&
+        mean_iter != mean_end){
     if(dim[1] - *na_count_iter - 1 < 0){
       *res_iter = R_NaN;
     }else{
